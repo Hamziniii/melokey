877157cf6e363bdb50cb2c801cc7eee7b9a1e5fc
Angel <22824872+Zyplos@users.noreply.github.com>
2023-10-28 01:23:22 -0500

    user login works, weird cookie bug though

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import nodejs from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: nodejs({
+    mode: "standalone",
+  }),
 });
