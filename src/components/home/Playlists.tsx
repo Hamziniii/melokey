9578fc8af8d7b2be7aee639c6d16fa69fa372576
Hamza G. <hamzagaziuddin04@gmail.com>
2023-10-31 02:15:56 -0500

    Slight changes

--- src/components/home/Playlists.tsx
+++ src/components/home/Playlists.tsx
@@ -31,7 +31,7 @@ export default function Playlists({sdkProps, playlistCount = 4}: {sdkProps: SdkP
     const image = playlist.images[0]?.url
   
     return (
-      <div playlist-id={playlist.id} className="my-2 mx-2 p-4 transition-colors duration-200 ease-in-out bg-transparent rounded-lg hover:bg-[#ffffff0a] cursor-pointer"
+      <div playlist-id={playlist.id} key={playlist.id} className="my-2 mx-2 p-4 transition-colors duration-200 ease-in-out bg-transparent rounded-lg hover:bg-[#ffffff0a] cursor-pointer"
         onClick={() => window.location.href = `/playlist-viewer/${playlist.id}`}>
         {
           image ? (
