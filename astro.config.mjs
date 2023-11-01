5a9ffa48a23f1230b7306a7a88f2989e41c49cc1
Hamza G. <hamzagaziuddin04@gmail.com>
2023-11-01 14:01:42 -0500

    build for vercel?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import nodejs from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: nodejs({
-    mode: "standalone",
-  }),
+  adapter: vercel()
 });
