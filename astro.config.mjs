71372cea1b397b6a35cb73f60286fd9097cacb90
Hamza G <hamzagaziuddin04@gmail.com>
2023-11-01 14:04:44 -0500

    Revert "build for vercel?"
    This reverts commit 5a9ffa48a23f1230b7306a7a88f2989e41c49cc1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import nodejs from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+
+import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: nodejs({
+    mode: "standalone",
+  }),
 });
